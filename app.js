27cf65b52a30b2c793095715ec28a0a0c52e00be
hannahxxo <hannahantle@gmail.com>
2021-03-29 09:34:10 -0500

    Changes

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 // ASYNC BASICS EXERCISE
 // 1
 let name1 = "Falcon";
+
 function falconName() {
   console.log(`Question 1`);
   console.log(name1);
 }
+
 function falcon(n, callback) {
   setTimeout(() => {
     name1 = n;
-    callback(n)
+    callback();
   }, 1000);
 }
-falcon(`Sam Wilson`);
-falconName();
+falcon(`Sam Wilson`, falconName);
 
 // 2
 let name2 = "Winter Soldier";
+
 function winterSoldierName() {
   console.log(`Question 2`);
   console.log(name2);
 }
 function winterSoldier(n) {
     return new Promise((resolve, reject) => {
-  setTimeout(() => {
-    name2 = n;
-  }, 2000);
+        setTimeout(() => {
+            name2 = n;
+            resolve();
+        }, 2000);
+  });
 }
+
 winterSoldier(`Bucky Barnes`)
     .then(winterSoldierName);
 
+// 3
 // 3
 let name3 = "Agent 13";
 function agent13Name() {
@@ -39,8 +45,13 @@ function agent13(n){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             name3 = n;
-        }, 3000);
-    })
+            resolve();
+    }, 3000);
+  });
 }
-agent13('Sharon Carter');
-agent13Name();
+
+async function func1(){
+    await agent13(`Sharon Carter`);
+    agent13Name();
+  }
+  func1();
